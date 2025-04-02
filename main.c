b17b1bcdeb66dd9f8800b06716f60041eac07337
闻权 <wenquan@enlink.cn>
2025-04-02 11:16:17 +0800

    增加日志方便定位文件挂载耗时

--- main.c
+++ main.c
@@ -8448,6 +8448,7 @@ int main (int argc, char *argv[])
       error (0, errno, "cannot mount");
       goto err_out3;
     }
+  fprintf (stderr, "fuse-overlayfs mount success");
   fuse_daemonize (opts.foreground);
 
   if (lo.threaded)
